fbb4ec535cb9058558a07ff3871fef87c6ab88b6
berkanbasaran05 <77754197+berkanbasaran05@users.noreply.github.com>
2023-05-26 18:00:59 +0300

    last

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,23 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    
-       <div className="flex flex-col w-full  items-center justify-between min-h-screen bg-gray-100">
+       
+       <div className="flex flex-col w-full items-center p-10  min-h-screen h-full justify-between">
+        <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="absolute inset-0 w-full h-full  md:object-cover object-cover z-0"
+          >
+            <source src="/assets/banner/bannervideo.mp4" type="video/mp4" />
+          </video>
       <Head>
         <title>Coming Soon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-       <Header/>
+      <Header/>
+      
        <HomeHero/>
        <Footer/>
 
--- components/Home/HomeHero.tsx
+++ components/Home/HomeHero.tsx
@@ -9,18 +9,10 @@ const HomeHero = (props: Props) => {
 
    
       return (
-        <div className='flex flex-col w-full h-full items-center mx-auto'>
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            className="absolute inset-0 w-full h-full md:object-cover object-cover z-0"
-          >
-            <source src="/assets/banner/bannervideo.mp4" type="video/mp4" />
-          </video>
+        <div className='flex flex-col w-full h-full items-center justify-center'>
+          
            
-          <div className='flex flex-col justify-between items-center max-w-[800px]  backdrop-blur-md shadow-2xl p-20 rounded-2xl'> 
+          <div className='flex flex-col justify-between items-center   backdrop-blur-md shadow-2xl  p-20 rounded-2xl'> 
           <h4 className='relative text-5xl lg:text-[90px]  text-white font-extralight font-serif' >CafeYakamoz</h4>
  
         <h1 className='text-3xl lg:text-3xl text-white mt-4 shadow-2xl relative font-extralight font-serif scroll-px-10'>
