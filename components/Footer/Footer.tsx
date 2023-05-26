57df75689783c451a96ca5ad1fad93a129298928
berkanbasaran05 <77754197+berkanbasaran05@users.noreply.github.com>
2023-05-26 17:23:25 +0300

    last

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer className="mt-8 text-white blur-[1px] relative">
+    <p>&copy; 2023 CafeYakamoz. All Right Reserved.</p>
+  </footer>
+  )
+}
+
+export default Footer
---
+++ components/Header/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <div className='mt-4 flex flex-row items-center p-4'>
+        {/**<img src="/logo.png" alt="" className="w-12 h-12 mr-2"/> */}
+
+        <div className="flex flex-row"> </div> 
+    
+     </div>
+  )
+}
+
+export default Header
